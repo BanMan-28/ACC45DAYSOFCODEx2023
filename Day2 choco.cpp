ab57cb43ee5fe50753d1aa352c6bd0da42861db2
Manish Bansal <116427742+BanMan-28@users.noreply.github.com>
2023-09-19 19:35:58 +0530

    Add files via upload

---
+++ Day2 choco.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,X,Y,Z,a;
+	cin>>T;
+	cin>>X>>Y>>Z;
+	
+	for(int i=0;i<T;i++){
+	    a =( (5*X) + (10*Y))/Z;
+	    cout<<a ;
+	}
+	return 0;
+}
+
