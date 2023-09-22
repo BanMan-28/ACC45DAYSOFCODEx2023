0509e4226e2a7b4692ae6b479d10c7b929b04191
Manish Bansal <116427742+BanMan-28@users.noreply.github.com>
2023-09-22 22:26:31 +0530

    Add files via upload

---
+++ day 5 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,X,i;
+	cin>>T;
+	while(T--){
+	    cin>>X;
+	    if(X>15){
+	        cout<<"No";
+	    }
+	        else
+	        cout<<"Yes";
+	    }
+	
+	return 0;
+}
---
+++ day 5 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int N;
+	    cout<<"Enter an odd positive integer or number of odd hoops"<<endl;
+	    cin>>N;
+	    cout<<(N+1)/2;
+	}
+	return 0;
+}
