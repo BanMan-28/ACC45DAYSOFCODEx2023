80c9cb4f37c2aaa96f6db24eaf55654c5f6ebe28
Manish Bansal <116427742+BanMan-28@users.noreply.github.com>
2023-09-25 20:08:05 +0530

    Add files via upload

---
+++ BATH day8 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int X,Y;
+	    cin>>X>>Y;
+	    int a = X/(Y*2);
+	    cout<<a<<endl;
+	}
+	return 0;
+}
