49398565829ede0b199a98e3a58ecf1340ff03cf
Manish Bansal <116427742+BanMan-28@users.noreply.github.com>
2023-10-27 23:20:34 +0530

    Add files via upload

---
+++ ADACRA day40 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    string s; cin>>s;
+	    
+	    int n = s.size();
+	    int cnt=0;
+	    for(int i=0;i<n;i++){
+	        if(s[i]!=s[i+1]) cnt++;
+	    }
+	    cout<<cnt/2<<endl;
+	}
+}
